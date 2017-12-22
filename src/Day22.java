3b626a1a5bacacf26a7daa807a7318bdc41e1219
Daniel Rejment <daniel.rejment@tacton.com>
2017-12-22 08:00:00 +0100

    Cleanup

--- src/Day22.java
+++ src/Day22.java
@@ -11,84 +11,53 @@ import java.util.Map;
 public class Day22 {
     public static void main(String[] args) throws IOException {
         List<String> strings = Files.readAllLines(Paths.get("src/day22.txt"));
-        Map<Point, Integer> grid1 = new HashMap<>();
-        Map<Point, Integer> grid2 = new HashMap<>();
-        for (int i = 0; i < strings.size(); i++) {
-            String line = strings.get(i);
-            for (int j = 0; j < line.length(); j++) {
-                grid1.put(Point.of(j, i), line.charAt(j) == '#' ? 1 : 0);
-                grid2.put(Point.of(j, i), line.charAt(j) == '#' ? 2 : 0);
+        Map<Integer, Integer> grid1 = new HashMap<>();
+        Map<Integer, Integer> grid2 = new HashMap<>();
+        for (int y = 0; y < strings.size(); y++) {
+            String line = strings.get(y);
+            for (int x = 0; x < line.length(); x++) {
+                int key = (y + 10000) * 10000 + x;
+                grid1.put(key, line.charAt(x) == '#' ? 1 : 0);
+                grid2.put(key, line.charAt(x) == '#' ? 2 : 0);
             }
         }
 
-        Point current = Point.of(12, 12);
-        Point direction = Point.of(0, -1);
+        int[] dx = {0, 1, 0, -1};
+        int[] dy = {-1, 0, 1, 0};
+        int x = 12, y = 12;
+        int d = 0;
+
         int part1 = 0;
         for (int i = 0; i < 10000; i++) {
-            int val = grid1.getOrDefault(current, 0);
-            direction = direction.rotate90(val == 1 ? -1 : 1);
-            val = 1 - val;
-            grid1.put(current, val);
-            current = current.add(direction);
-            if (val == 1) part1++;
+            int key = (y + 10000) * 10000 + x;
+            int val = grid1.getOrDefault(key, 0);
+            d = (d + (val == 1 ? 1 : 3)) % 4;
+            grid1.put(key, 1 - val);
+            x += dx[d];
+            y += dy[d];
+            if (val == 0) part1++;
         }
         System.out.println(part1);
 
-        current = Point.of(12, 12);
-        direction = Point.of(0, -1);
+        x = y = 12;
+        d = 0;
         int part2 = 0;
         for (int i = 0; i < 10000000; i++) {
-            int inf = grid2.getOrDefault(current, 0);
-            if (inf == 0) {
-                grid2.put(current, 1);
-                direction = direction.rotate90(1);
-            } else if (inf == 1) {
-                grid2.put(current, 2);
+            int key = (y + 10000) * 10000 + x;
+            int val = grid2.getOrDefault(key, 0);
+            if (val == 0) {
+                d = (d + 3) % 4;
+            } else if (val == 1) {
                 part2++;
-            } else if (inf == 2) {
-                grid2.put(current, 3);
-                direction = direction.rotate90(-1);
-            } else if (inf == 3) {
-                grid2.put(current, 0);
-                direction = Point.of(-direction.x, -direction.y);
+            } else if (val == 2) {
+                d = (d + 1) % 4;
+            } else if (val == 3) {
+                d = (d + 2) % 4;
             }
-            current = current.add(direction);
+            grid2.put(key, (val + 1) % 4);
+            x += dx[d];
+            y += dy[d];
         }
         System.out.println(part2);
-    }
-
-    private static class Point {
-        int x;
-        int y;
-
-        static Point of(int x, int y) {
-            Point pt = new Point();
-            pt.x = x;
-            pt.y = y;
-            return pt;
-        }
-
-        Point rotate90(int direction) {
-            return Point.of(direction * y, (-direction) * x);
-        }
-
-        Point add(Point pt) {
-            return Point.of(x + pt.x, y + pt.y);
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            Point point = (Point) o;
-            return x == point.x && y == point.y;
-        }
-
-        @Override
-        public int hashCode() {
-            int result = x;
-            result = 31 * result + y;
-            return result;
-        }
     }
 }
